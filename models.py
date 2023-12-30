09edd92c57dfb2e6c489f35e3dc1e0db438c5dcc
Joe Philip <joe.philip@hotmail.co.in>
2023-12-31 01:29:49 +0530

    defined Profile model

--- models.py
+++ models.py
@@ -11,3 +11,10 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     phone = Column(String, unique=True, index=True)
+
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    profile_picture = Column(String)
