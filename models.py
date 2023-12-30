243ef07b4f34503c856eaaaadfbce41a542fc9c5
Joe Philip <joe.philip@hotmail.co.in>
2023-12-31 01:29:36 +0530

    defined User model

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    phone = Column(String, unique=True, index=True)
